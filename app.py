9f44b29b1182d035dabd9c6e6ad007c4a744d4a1
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-21 11:46:44 +0000

    Added the Delete service interface, some helper functions, and comments.

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, make_response, abort
+
+"""
+
+This is the main module for the registry service.  It uses a Flask and a
+simple in memory solution to demo the restful API.
+
+"""
+
 import http_status
+from flask import Flask, jsonify, request, make_response, abort
 
 app = Flask(__name__)
 
 SERVICES = {}
 
-@app.errorhandler(http_status.NOT_FOUND )
+
+@app.errorhandler(http_status.NOT_FOUND)
 def not_found(error):
     return make_response(
         jsonify({
             'error': 'Not found'
         }), http_status.NOT_FOUND)
 
-# This call is not in the requirements; it is for developement purposes only.
+
+# NOTE: This is not in the requirements; it is for developement purposes only.
 @app.route('/services/v1.0', methods=['GET'])
 def list_services():
-    """ Retrieves a list of all services. (N.B. FOR DEV VERSION ONLY) """
+    """Retrieves a list of all services. (N.B. FOR DEV VERSION ONLY)"""
     return make_response(jsonify({'services': SERVICES}), http_status.OK)
 
 
 @app.route('/services/v1.0', methods=['POST'])
 def add_service():
-    """ Creates a service.  """
+    """Creates a service."""
     if not request.json \
         or not 'service_name' in request.json \
         or not 'service_version' in request.json:
@@ -31,8 +41,8 @@ def add_service():
     service_name = request.json['service_name']
     service_version = request.json['service_version']
 
-    key = service_name + service_version
-    entry = SERVICES.get(key, {})
+    key = get_key(service_name, service_version)
+    entry = get_entry(key)
 
     if entry:
         entry['count'] += 1
@@ -47,7 +57,47 @@ def add_service():
     resp = dict(entry)
     resp['change'] = "created"
     resp.pop('count', None)
-    return make_response(jsonify({'resp': resp}),http_status.CREATED)
+    return make_response(jsonify({'resp': resp}), http_status.CREATED)
+
+
+@app.route('/services/v1.0/<service_name>', methods=['DELETE'])
+def delete_service(service_name):
+    """ Deletes a service. """
+    resp, http_return_status = delete_service_records(service_name)
+    return make_response(jsonify({'resp': resp}), http_return_status)
+
+
+def delete_service_records(service_name):
+    keys_to_delete = []
+    for k, v in SERVICES.items():
+        if v['service'] == service_name:
+            keys_to_delete.append(get_key(v['service'], v['version']))
+
+    service_deleted = False
+    for key in keys_to_delete:
+        del SERVICES[key]
+        service_deleted = True
+
+    resp = {}
+    http_return_status = http_status.OK
+    resp['service'] = service_name
+
+    if service_deleted:
+        resp['change'] = 'removed'
+    else:
+        resp['change'] = 'notfound'
+        http_return_status = http_status.NOT_FOUND
+
+    return resp, http_return_status
+
+
+def get_entry(key):
+    return SERVICES.get(key, {})
+
+
+def get_key(service, version):
+    return service + '-v' + version
+
 
 if __name__ == '__main__':
     app.run(debug=True)
