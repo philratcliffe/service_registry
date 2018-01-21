a642a21652f29f36e478f3e4cc9b5769d8c3fd05
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-21 11:11:45 +0000

    Added a test for invalid POST params and also a test to check handling of an invalid URL.

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import json
+import http_status
 
 
 class TestServiceRegistry(unittest.TestCase):
@@ -23,8 +24,35 @@ class TestServiceRegistry(unittest.TestCase):
             }
         }
 
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, http_status.CREATED)
         self.assertEqual(response.json(), exptected_result)
+
+    def test_invalid_add_service(self):
+        data = {'srvc_nam': 'test', 'service_version': '0.0.1'}
+        data_json = json.dumps(data)
+        headers = {'Content-type': 'application/json'}
+
+        response = requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+
+        self.assertEqual(response.status_code, http_status.BAD_REQUEST)
+
+
+    def test_invalid_url(self):
+        data = {'service_name': 'test', 'service_version': '0.0.1'}
+        data_json = json.dumps(data)
+        headers = {'Content-type': 'application/json'}
+
+        response = requests.post(
+            'http://localhost:5000/sevices/v1.0',
+            data=data_json,
+            headers=headers)
+
+        self.assertEqual(response.status_code, http_status.NOT_FOUND)
+
+
 
 if __name__ == "__main__":
     unittest.main()
