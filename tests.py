fe9b327cdee28795444aea800cc214731fad8261
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-21 11:44:13 +0000

    Added delete test.

--- tests.py
+++ tests.py
@@ -1,10 +1,72 @@
+"""
+
+This module contains the tests for registry service.
+
+"""
+
 import unittest
 import requests
 import json
+
 import http_status
 
-
 class TestServiceRegistry(unittest.TestCase):
+
+    def setUp(self):
+        """ Populate the service registry. """
+
+        #
+        # Add test 0.0.1 (twice)
+        #
+        headers = {'Content-type': 'application/json'}
+        data = {'service_name': 'test', 'service_version': '0.0.1'}
+        data_json = json.dumps(data)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+
+        #
+        # Add test 0.0.2 (twice)
+        #
+        headers = {'Content-type': 'application/json'}
+        data = {'service_name': 'test', 'service_version': '0.0.2'}
+        data_json = json.dumps(data)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+
+        #
+        # Add test2 0.0.2 (twice)
+        #
+        headers = {'Content-type': 'application/json'}
+        data = {'service_name': 'test2', 'service_version': '0.0.2'}
+        data_json = json.dumps(data)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+        requests.post(
+            'http://localhost:5000/services/v1.0',
+            data=data_json,
+            headers=headers)
+
+    def tearDown(self):
+        """ Delete services from service registry. """
+        requests.delete(
+            'http://localhost:5000/services/v1.0/test')
+        requests.delete(
+            'http://localhost:5000/services/v1.0/test2')
+
 
     def test_add_service(self):
         data = {'service_name': 'test', 'service_version': '0.0.1'}
@@ -51,6 +113,21 @@ class TestServiceRegistry(unittest.TestCase):
             headers=headers)
 
         self.assertEqual(response.status_code, http_status.NOT_FOUND)
+
+
+    def test_delete_service(self):
+        response = requests.delete(
+            'http://localhost:5000/services/v1.0/test')
+
+        exptected_result = {
+            "resp": {
+                "change": "removed",
+                "service": "test"
+            }
+        }
+
+        self.assertEqual(response.status_code, http_status.OK)
+        self.assertEqual(response.json(), exptected_result)
 
 
 
