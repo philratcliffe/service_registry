e2d5f6e43797cf84de38d6d49ae0c20c2644fbf1
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-21 13:36:58 +0000

    Added the find service.

--- app.py
+++ app.py
@@ -67,6 +67,76 @@ def delete_service(service_name):
     return make_response(jsonify({'resp': resp}), http_return_status)
 
 
+@app.route(
+    '/services/v1.0/<service_name>',
+    defaults={'service_version': None},
+    methods=['GET'])
+@app.route('/services/v1.0/<service_name>/<service_version>', methods=['GET'])
+def find_service(service_name, service_version):
+    """
+    Retrieves a service using the service name and optionally a version.
+    """
+    if service_version:
+        resp = find_service_with_version(service_name, service_version)
+        return make_response(jsonify({'resp': resp}), http_status.OK)
+
+    resp = find_service_without_version(service_name)
+    return make_response(jsonify({'resp': resp}), http_status.OK)
+
+
+def find_service_with_version(service_name, service_version):
+    key = get_key(service_name, service_version)
+    if key in SERVICES:
+        return SERVICES[key]
+    else:
+        return {
+            'service': service_name,
+            'version': service_version,
+            'count': 0
+        }
+
+
+def find_service_without_version(service_name):
+    resp = {'service': service_name, 'count': 0}
+    print(SERVICES)
+    for k, v in SERVICES.items():
+        if v['service'] == service_name:
+            resp['count'] += v['count']
+
+    return resp
+
+
+def delete_service_records(service_name):
+    keys_to_delete = []
+    for k, v in SERVICES.items():
+        if v['service'] == service_name:
+            keys_to_delete.append(get_key(v['service'], v['version']))
+
+    service_deleted = False
+    for key in keys_to_delete:
+        del SERVICES[key]
+        service_deleted = True
+
+    resp = {}
+    http_return_status = http_status.OK
+    resp['service'] = service_name
+
+    if service_deleted:
+        resp['change'] = 'removed'
+    else:
+        resp['change'] = 'notfound'
+        http_return_status = http_status.NOT_FOUND
+
+    return resp, http_return_status
+
+
+def get_entry(key):
+    return SERVICES.get(key, {})
+
+
+def get_key(service, version):
+    return service + '-v' + version
+
 def delete_service_records(service_name):
     keys_to_delete = []
     for k, v in SERVICES.items():
