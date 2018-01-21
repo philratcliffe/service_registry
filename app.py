90cd400b23fa54bbb4fa11d82e2b24927932ee1e
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-21 11:09:39 +0000

    Added some input validation and error handling. Used http_status.

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
+import http_status
 
 app = Flask(__name__)
 
 SERVICES = {}
 
+@app.errorhandler(http_status.NOT_FOUND )
+def not_found(error):
+    return make_response(
+        jsonify({
+            'error': 'Not found'
+        }), http_status.NOT_FOUND)
 
 # This call is not in the requirements; it is for developement purposes only.
 @app.route('/services/v1.0', methods=['GET'])
 def list_services():
     """ Retrieves a list of all services. (N.B. FOR DEV VERSION ONLY) """
-    return make_response(jsonify({'services': SERVICES}), 200)
+    return make_response(jsonify({'services': SERVICES}), http_status.OK)
 
 
 @app.route('/services/v1.0', methods=['POST'])
 def add_service():
     """ Creates a service.  """
+    if not request.json \
+        or not 'service_name' in request.json \
+        or not 'service_version' in request.json:
+        abort(http_status.BAD_REQUEST)
+
     service_name = request.json['service_name']
     service_version = request.json['service_version']
 
@@ -35,7 +47,7 @@ def add_service():
     resp = dict(entry)
     resp['change'] = "created"
     resp.pop('count', None)
-    return make_response(jsonify({'resp': resp}), 201)
+    return make_response(jsonify({'resp': resp}),http_status.CREATED)
 
 if __name__ == '__main__':
     app.run(debug=True)
